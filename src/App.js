228ccfb1dc04e98bfd37729d91668d9853b91f90
Deivid Guauña <deividj.54@gmail.com>
2022-05-12 15:47:35 -0500

    agregando estilos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Field, Formik, Form } from 'formik'
 import React from 'react'
+import './header.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <header>
+            <Formik
+            initialValues={{ search: '' }}
+            onSubmit={ async values=>{
+                //llamar a api de unplash
+                console.log(values)
+            } }            
+            >
+                <Form>
+                    <Field name="search"/>
+                </Form>
+            </Formik>    
+        </header>    
+    </div>
   )
 }
 
